3e49a76985788ea7631a8fc0cd007e34f3e179fd
Joe Scotto <contact@joescotto.com>
2022-10-04 13:08:41 -0400

    Fix ScottoAlp

--- ScottoAlp/QMK/keymaps/default/keymap.c
+++ ScottoAlp/QMK/keymaps/default/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,    KC_F,    KC_P,    KC_G,   KC_J, KC_L,          KC_U,          KC_Y,   KC_BSPC,
         KC_A,         KC_R,    KC_S,    KC_T,    KC_D,   KC_H, KC_N,          KC_E,          KC_I,   KC_O,
         LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,   KC_K, KC_M,          KC_COMMA,      KC_DOT, RSFT_T(KC_SLSH),
-                      KC_LALT, KC_LCTL,          KC_SPC,       LT(5, KC_TAB), LT(6, KC_ENT),
+                      KC_LALT, KC_LCTL,          KC_SPC,       LT(5, KC_TAB), LT(6, KC_ENT)
     ),
     [5] = LAYOUT_ortho_3x10_5(
         KC_UNDS,         KC_MINS, KC_PLUS, KC_EQL,  KC_COLN, KC_GRV,   KC_MRWD,       KC_MPLY,      KC_MFFD, KC_DEL,

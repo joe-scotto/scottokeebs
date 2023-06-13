97db5296117ae214d3cf08599cbec74eba023079
Joe Scotto <contact@joescotto.com>
2023-06-12 21:30:02 -0400

    Finalize ScottoCMD

--- ScottoCMD/QMK/keymaps/vial/config.h
+++ ScottoCMD/QMK/keymaps/vial/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Joe Scotto
+Copyright 2023 Joe Scotto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,5 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// OLED
-#define OLED_DISPLAY_128X64
+// Define options
+#define TAPPING_TERM 135
+#define PERMISSIVE_HOLD
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x10, 0x0D, 0xAB, 0x63, 0x89, 0xC2, 0xA1, 0x54}
+#define VIAL_UNLOCK_COMBO_ROWS { 4, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 10 }

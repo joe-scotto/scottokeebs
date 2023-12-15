b68f65e729f6312cf23eb0ac6dec5a67703a3336
Joe Scotto <contact@joescotto.com>
2023-12-14 19:21:54 -0500

    Add Scotto61

---
+++ Scotto61/QMK/keymaps/vial/config.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2023 Joe Scotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x9B, 0x20, 0x8B, 0x86, 0x34, 0x80, 0x6B, 0x46}
+#define VIAL_UNLOCK_COMBO_ROWS { 4, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+// RGB
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGB_DI_PIN 23
---
+++ Scotto61/README.md
@@ -0,0 +1,7 @@
+# Scotto61
+
+The Scotto61 is a 61-key ortholinear keyboard with a 7u spacebar.
+
+You can read more about the components and how to build it [here](https://scottokeebs.com/blogs/keyboards/scotto61-handwired-keyboard).
+
+![Scotto61](https://github.com/joe-scotto/scottokeebs/assets/8194147/50dba991-3084-44c9-9ea5-16cca23c79b1)

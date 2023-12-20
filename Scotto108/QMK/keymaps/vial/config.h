db3c6ed61f65ece785b39191bf86c4842f8b2d50
Joe Scotto <contact@joescotto.com>
2023-12-19 23:32:41 -0500

    Add Scotto108

---
+++ Scotto108/QMK/keymaps/vial/config.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2023 Joe Scotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x8B, 0x9B, 0xB5, 0xD3, 0x69, 0xB0, 0x27, 0x05}
+#define VIAL_UNLOCK_COMBO_ROWS { 5, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 20 }
+
+// RGB
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGB_DI_PIN 23
---
+++ Scotto108/README.md
@@ -0,0 +1,7 @@
+# Scotto108
+
+The Scotto108 is a 108-key full-sized ortholinear keyboard with a 7u spacebar and 4 macro keys above the numpad.
+
+You can read more about the components and how to build it [here](https://scottokeebs.com/blogs/keyboards/scotto108-handwired-keyboard).
+
+<!-- ![Scotto108](https://github.com/joe-scotto/scottokeebs/assets/8194147/3f172306-9089-40ce-93ef-c791886ddd4e) -->

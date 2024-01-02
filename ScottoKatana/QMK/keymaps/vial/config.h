404627f458a1aec3d92b032ee65618e050e81090
Joe Scotto <contact@joescotto.com>
2024-01-01 23:28:21 -0500

    Add ScottoKatana

---
+++ ScottoKatana/QMK/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/*
+Copyright 2024 Joe Scotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Define options
+#define TAPPING_TERM 135
+#define PERMISSIVE_HOLD
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x7A, 0x60, 0x84, 0x17, 0x10, 0x1C, 0x36, 0xCE}
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 8 }
---
+++ ScottoKatana/README.md
@@ -0,0 +1,7 @@
+# ScottoKatana
+
+The ScottoKatana is a 33-key katana staggered keyboard with a 6.25u spacebar and two 1.5u function keys.
+
+You can read more about the components and how to build it [here](https://scottokeebs.com/blogs/keyboards/scottokatana-handwired-keyboard).
+
+![ScottoKatana](https://i.imgur.com/pgXehiIh.jpeg)

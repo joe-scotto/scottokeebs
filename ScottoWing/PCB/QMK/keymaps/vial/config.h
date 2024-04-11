aa3766abae7df00eab4a220b4cb8be69c8a3482b
Joe Scotto <contact@joescotto.com>
2024-04-11 11:11:23 -0400

    Add ScottoWing (PCB Edition) firmware

---
+++ ScottoWing/PCB/QMK/keymaps/vial/config.h
@@ -0,0 +1,31 @@
+/*
+Copyright 2024 Joe Scotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Define options
+#define TAPPING_TERM 135
+#define PERMISSIVE_HOLD
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x5A, 0x08, 0x0E, 0x94, 0xFE, 0x79, 0x6B, 0x3E}
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 7 }
+
+// Bootmagic
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 1
---
+++ ScottoWing/PCB/README.md
@@ -0,0 +1,7 @@
+# ScottoWing (PCB Edition)
+
+The ScottoWing (PCB Edition) is a 3x5 (34-keys) or 3x6 (40-keys) split monoblock ergonomic column-staggered keyboard.
+
+You can read more about the components and how to build it [here](https://scottokeebs.com/blogs/keyboards/scottowing-pcb-keyboard).
+
+![ScottoWing](https://github.com/joe-scotto/scottokeebs/assets/8194147/5dc4ec52-acaf-4804-8037-94ab414726e2)

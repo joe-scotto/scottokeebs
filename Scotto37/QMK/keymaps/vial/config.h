b199db8717030668c880e9529b3a1b3f31bf7659
Joe Scotto <contact@joescotto.com>
2024-06-16 16:08:20 -0400

    Working Scotto37

---
+++ Scotto37/QMK/keymaps/vial/config.h
@@ -0,0 +1,28 @@
+/*
+Copyright 2024 Joe Scotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Define options
+#define TAPPING_TERM 135
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x1F, 0xAF, 0xF4, 0x5A, 0x17, 0x11, 0xDA, 0x7F}
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
---
+++ Scotto37/README.md
@@ -0,0 +1,7 @@
+# Scotto37 (SK27)
+
+The Scotto34 is a 37-key gasket-mounted 0.25u column-staggered keyboard with a 3u spacebar.
+
+You can read more about the components and how to build it [here](https://scottokeebs.com/blogs/keyboards/Scotto37-handwired-keyboard).
+
+![Scotto37](https://user-images.githubusercontent.com/8194147/205760764-cce990f6-0c81-4971-ae52-ec44b2bc3c33.jpg)

126f9bca05bed338201273b0d76992bdb2256b66
Joe Scotto <contact@joescotto.com>
2025-09-09 15:49:57 -0400

    Add code combo also

--- Scotto34/Handwired/QMK/keymaps/scotto/combos.c
+++ Scotto34/Handwired/QMK/keymaps/scotto/combos.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
-#include "keycodes.c"
+
+enum custom_keycodes {
+  OS_TOGGLE = SAFE_RANGE,
+  TO_CODE,
+  TO_NUMBER,
+  TO_DEFAULT
+};
 
 // Combos
 const uint16_t PROGMEM tab_combo[] = {KC_Y, KC_BSPC, COMBO_END};
@@ -9,26 +15,34 @@ const uint16_t PROGMEM enter_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM enter_number_combo[] = {KC_EXLM, KC_AT, COMBO_END};
 const uint16_t PROGMEM enter_code_combo[] = {KC_UNDS, KC_MINS, COMBO_END};
 
-const uint16_t PROGMEM number_combo[] = {KC_Q, KC_BSPC, COMBO_END};
-const uint16_t PROGMEM number_number_combo[] = {KC_EXLM, KC_BSPC, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(tab_combo, KC_TAB),          COMBO(tab_number_combo, KC_TAB),
-    COMBO(tab_code_combo, KC_TAB),     COMBO(enter_combo, KC_ENT),
-    COMBO(enter_number_combo, KC_ENT), COMBO(enter_code_combo, KC_ENT),
-    COMBO(number_combo, TO_NUMBER),    COMBO(number_number_combo, TO_DEFAULT),
-};
+const uint16_t PROGMEM number_combo[] = {KC_W, KC_Y, COMBO_END};
+const uint16_t PROGMEM number_number_combo[] = {KC_AT, KC_CAPS, COMBO_END};
+
+const uint16_t PROGMEM code_combo[] = {KC_Q, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM code_code_combo[] = {KC_UNDS, KC_DEL, COMBO_END};
+combo_t key_combos[] = {COMBO(tab_combo, KC_TAB),          COMBO(tab_number_combo, KC_TAB),
+                        COMBO(tab_code_combo, KC_TAB),     COMBO(enter_combo, KC_ENT),
+                        COMBO(enter_number_combo, KC_ENT), COMBO(enter_code_combo, KC_ENT),
+                        COMBO(number_combo, TO_NUMBER),    COMBO(number_number_combo, TO_DEFAULT),
+                        COMBO(code_combo, TO_CODE),        COMBO(code_code_combo, TO_DEFAULT)};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case TO_NUMBER:
-      if (record->event.pressed) {
-        layer_move(2);
-      }
-      return false;
-      break;
     case TO_DEFAULT:
       if (record->event.pressed) {
         layer_move(0);
+      }
+      return false;
+      break;
+    case TO_CODE:
+      if (record->event.pressed) {
+        layer_move(1);
+      }
+      return false;
+      break;
+    case TO_NUMBER:
+      if (record->event.pressed) {
+        layer_move(2);
       }
       return false;
       break;
--- Scotto34/Handwired/QMK/keymaps/scotto/keycodes.c
+++
@@ -1,8 +0,0 @@
-#include QMK_KEYBOARD_H
-
-enum custom_keycodes {
-  OS_TOGGLE = SAFE_RANGE,
-  TO_CODE,
-  TO_NUMBER,
-  TO_DEFAULT
-};

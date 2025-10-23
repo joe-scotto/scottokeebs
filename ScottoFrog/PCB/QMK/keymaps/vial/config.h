b1d48b86bdf742df45f53cbbc32577e892f68bdf
Joe Scotto <contact@joescotto.com>
2025-10-23 16:14:10 -0400

    Finish up firmware

--- ScottoFrog/PCB/QMK/keymaps/vial/config.h
+++ ScottoFrog/PCB/QMK/keymaps/vial/config.h
@@ -1,23 +1,6 @@
-/*
-Copyright 2023 Joe Scotto
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 // Vial
-#define VIAL_KEYBOARD_UID {0x6A, 0xF5, 0x1C, 0x51, 0x91, 0xE5, 0xE3, 0x33}
+#define VIAL_KEYBOARD_UID {0xB1, 0x90, 0xAE, 0xFD, 0xD9, 0x0F, 0x78, 0x4C}
 #define VIAL_UNLOCK_COMBO_ROWS {3, 3}
 #define VIAL_UNLOCK_COMBO_COLS {0, 4}

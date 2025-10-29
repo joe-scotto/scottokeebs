0e754e3f9498fb405f999ce1cfc84ca4064799e2
Joe Scotto <contact@joescotto.com>
2025-10-29 11:39:00 -0400

    Update left

--- ScottoFrog/PCB/QMK/custom_keys.c
+++ ScottoFrog/PCB/QMK/custom_keys.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+// Force num lock always on
+void keyboard_post_init_user(void) {
+  if (!host_keyboard_led_state().num_lock) {
+    register_code(KC_NUM_LOCK);
+    unregister_code(KC_NUM_LOCK);
+  }
+}
+
 enum custom_keycodes {
   KC_CARET = SAFE_RANGE
 };
@@ -10,6 +18,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         tap_code16(S(KC_6));
       }
+      return false;
+    case KC_NUM_LOCK:
       return false;
   }
   return true;
--- ScottoFrog/PCB/QMK/keymaps/left/keymap.c
+++ ScottoFrog/PCB/QMK/keymaps/left/keymap.c
@@ -1,20 +1,3 @@
-/*
-Copyright 2025 Joe Scotto
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 #include "combos.c"
 
@@ -43,20 +26,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TD(SFT_LOCK) , KC_ENTER ,       TO(2)        , TO(1) , KC_SPC
 ),
 
-//                   ┌─────┬───┬───┬───┬───────┐
-//                   │  -  │ 7 │ 8 │ 9 │ kp_-  │
-//                   ├─────┼───┼───┼───┼───────┤
-//                   │  +  │ 4 │ 5 │ 6 │   /   │
-//                   ├─────┼───┼───┼───┼───────┤
-//                   │  0  │ 1 │ 2 │ 3 │   =   │
-//    ┌──────────────┼─────┼───┴───┴───┼───────┼─────┐
-//    │ TD(SFT_LOCK) │ ent │   TO(2)   │ TO(0) │ spc │
-//    └──────────────┴─────┴───────────┴───────┴─────┘
+//                   ┌──────┬───┬───┬───┬───────┐
+//                   │ kp_* │ 7 │ 8 │ 9 │ kp_-  │
+//                   ├──────┼───┼───┼───┼───────┤
+//                   │ kp_/ │ 4 │ 5 │ 6 │ kp_+  │
+//                   ├──────┼───┼───┼───┼───────┤
+//                   │  0   │ 1 │ 2 │ 3 │   =   │
+//    ┌──────────────┼──────┼───┴───┴───┼───────┼─────┐
+//    │ TD(SFT_LOCK) │ ent  │   TO(2)   │ TO(0) │ spc │
+//    └──────────────┴──────┴───────────┴───────┴─────┘
 [NUMBER] = LAYOUT_ortho_4x5(
-                 KC_MINUS , KC_7 , KC_8 , KC_9 , KC_KP_MINUS         ,
-                 KC_PLUS  , KC_4 , KC_5 , KC_6 , KC_SLASH            ,
-                 KC_0     , KC_1 , KC_2 , KC_3 , KC_EQUAL            ,
-  TD(SFT_LOCK) , KC_ENTER ,       TO(2)        , TO(0)       , KC_SPC
+                 KC_KP_ASTERISK , KC_7 , KC_8 , KC_9 , KC_KP_MINUS         ,
+                 KC_KP_SLASH    , KC_4 , KC_5 , KC_6 , KC_KP_PLUS          ,
+                 KC_0           , KC_1 , KC_2 , KC_3 , KC_EQUAL            ,
+  TD(SFT_LOCK) , KC_ENTER       ,       TO(2)        , TO(0)       , KC_SPC
 ),
 
 //                   ┌───────────┬───┬───┬───┬───────┐

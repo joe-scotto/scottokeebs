b7639e05b4bcc7fe46711e5124e7b9211ca02f21
Joe Scotto <contact@joescotto.com>
2025-11-10 17:35:02 -0500

    working map

--- ScottoFrog/PCB/QMK/keymaps/default/keymap.c
+++ ScottoFrog/PCB/QMK/keymaps/default/keymap.c
@@ -1,43 +1,39 @@
+#include "combos.c"
 #include "tapdance.c"
 #include QMK_KEYBOARD_H
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // Space tap, 0 on hold
-    case LT(0, KC_SPC):
-      if (!record->tap.count && record->event.pressed) {
-        tap_code(KC_0);
-        return false;
-      }
-      return true;
-
-    // Backspace tap, # on hold
-    case LT(0, KC_BSPC):
-      if (!record->tap.count && record->event.pressed) {
-        tap_code(KC_2);
-        return false;
-      }
-      return true;
-  }
-  return true;
-}
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//         ┌─────────────────┬──────────┬──────────┬──────────┬─────────────┐
-//         │       no        │ TD(TD_1) │ TD(TD_2) │ TD(TD_3) │     no      │
-//         ├─────────────────┼──────────┼──────────┼──────────┼─────────────┤
-//         │       no        │ TD(TD_4) │ TD(TD_5) │ TD(TD_6) │     no      │
-//         ├─────────────────┼──────────┼──────────┼──────────┼─────────────┤
-//         │       no        │ TD(TD_7) │ TD(TD_8) │ TD(TD_9) │     no      │
-//    ┌────┼─────────────────┼──────────┴──────────┴──────────┼─────────────┼─────────┐
-//    │ no │ MT(MOD_LSFT, ') │           LT(0, spc)           │ LT(0, bspc) │ QK_BOOT │
-//    └────┴─────────────────┴────────────────────────────────┴─────────────┴─────────┘
+//         ┌──────┬──────────┬──────────┬──────────┬─────────────┐
+//         │  no  │ TD(TD_1) │ TD(TD_2) │ TD(TD_3) │     no      │
+//         ├──────┼──────────┼──────────┼──────────┼─────────────┤
+//         │  no  │ TD(TD_4) │ TD(TD_5) │ TD(TD_6) │     no      │
+//         ├──────┼──────────┼──────────┼──────────┼─────────────┤
+//         │  no  │ TD(TD_7) │ TD(TD_8) │ TD(TD_9) │     no      │
+//    ┌────┼──────┼──────────┴──────────┴──────────┼─────────────┼────┐
+//    │ no │ rsft │            TD(TD_0)            │ LT(1, bspc) │ no │
+//    └────┴──────┴────────────────────────────────┴─────────────┴────┘
 [0] = LAYOUT_ortho_4x5(
-          KC_NO                  , TD(TD_1) , TD(TD_2) , TD(TD_3) , KC_NO                   ,
-          KC_NO                  , TD(TD_4) , TD(TD_5) , TD(TD_6) , KC_NO                   ,
-          KC_NO                  , TD(TD_7) , TD(TD_8) , TD(TD_9) , KC_NO                   ,
-  KC_NO , MT(MOD_LSFT, KC_QUOTE) ,         LT(0, KC_SPC)          , LT(0, KC_BSPC) , QK_BOOT
+          KC_NO   , TD(TD_1) , TD(TD_2) , TD(TD_3) , KC_NO                 ,
+          KC_NO   , TD(TD_4) , TD(TD_5) , TD(TD_6) , KC_NO                 ,
+          KC_NO   , TD(TD_7) , TD(TD_8) , TD(TD_9) , KC_NO                 ,
+  KC_NO , KC_RSFT ,            TD(TD_0)            , LT(1, KC_BSPC) , KC_NO
+),
+
+//         ┌─────┬──────┬─────────┬──────┬─────┐
+//         │ no  │ esc  │   up    │ ent  │ no  │
+//         ├─────┼──────┼─────────┼──────┼─────┤
+//         │ no  │ left │  down   │ rght │ no  │
+//         ├─────┼──────┼─────────┼──────┼─────┤
+//         │ no  │ caps │ QK_BOOT │ del  │ no  │
+//    ┌────┼─────┼──────┴─────────┴──────┼─────┼─────┐
+//    │ no │     │                       │     │     │
+//    └────┴─────┴───────────────────────┴─────┴─────┘
+[1] = LAYOUT_ortho_4x5(
+          KC_NO   , KC_ESC  , KC_UP   , KC_ENTER  , KC_NO            ,
+          KC_NO   , KC_LEFT , KC_DOWN , KC_RIGHT  , KC_NO            ,
+          KC_NO   , KC_CAPS , QK_BOOT , KC_DELETE , KC_NO            ,
+  KC_NO , KC_TRNS ,            KC_TRNS            , KC_TRNS , KC_TRNS
 )
 };
 // clang-format on

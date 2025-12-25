d47455dbcd09e829896949eb2f77f1c931431180
Joe Scotto <contact@joescotto.com>
2025-12-25 18:01:09 -0500

    Use RCTL on game mode

--- Scotto40/QMK/keymaps/scotto/keymap.c
+++ Scotto40/QMK/keymaps/scotto/keymap.c
@@ -44,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├─────┼───┼───┼──────┼─────┼─────┼─────┼─────┼─────┼──────┤
 //    │  z  │ x │ c │  v   │  b  │  k  │  m  │  ,  │  .  │  /   │
 //    ├─────┼───┼───┼──────┼─────┼─────┼─────┼─────┼─────┼──────┤
-//    │     │ k │ j │ lctl │ spc │     │     │     │     │      │
+//    │     │ k │ j │ rctl │ spc │     │     │     │     │      │
 //    └─────┴───┴───┴──────┴─────┴─────┴─────┴─────┴─────┴──────┘
 [1] = LAYOUT_ortho_4x10(
   KC_Q    , KC_W , KC_F , KC_P    , KC_G   , KC_J    , KC_L    , KC_U     , KC_Y    , KC_BSPC,
   KC_A    , KC_R , KC_S , KC_T    , KC_D   , KC_H    , KC_N    , KC_E     , KC_I    , KC_O   ,
   KC_Z    , KC_X , KC_C , KC_V    , KC_B   , KC_K    , KC_M    , KC_COMMA , KC_DOT  , KC_SLSH,
-  KC_TRNS , KC_K , KC_J , KC_LCTL , KC_SPC , KC_TRNS , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS
+  KC_TRNS , KC_K , KC_J , KC_RCTL , KC_SPC , KC_TRNS , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS
 ),
 
 //    ┌───────────┬──────────────────┬─────┬─────┬─────┬─────┬──────┬──────┬──────────────┬───────────┐

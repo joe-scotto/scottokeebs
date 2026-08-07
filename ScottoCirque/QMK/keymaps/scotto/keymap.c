aef70bd5bf49859800342a1f989d2d60de19a47c
Joe Scotto <contact@joescotto.com>
2026-08-06 21:37:19 -0400

    Add ScottoCirque

---
+++ ScottoCirque/QMK/keymaps/scotto/keymap.c
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//    ┌────┐
+//    │ no │
+//    └────┘
+[0] = LAYOUT(
+  KC_NO
+)
+};
+// clang-format on
